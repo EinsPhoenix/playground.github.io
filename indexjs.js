587f1019438e85ecec505d9fd390dcef1713e524
EinsPhoenix <138807190+EinsPhoenix@users.noreply.github.com>
2023-09-09 13:13:21 +0200

    Delete indexjs.js

--- indexjs.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function () {
-    var navbar = document.getElementById('navbar');
-
-    if (window.scrollY > 100) { // Hier können Sie den Schwellenwert für das Scrollen anpassen
-        navbar.classList.remove('hidden');
-    } else {
-        navbar.classList.add('hidden');
-    }
-});
